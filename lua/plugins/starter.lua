1340864fd500a9d74c9d3dcfef6284c0b918bbf9
Mathiew82 <amateo1982@yahoo.es>
2026-01-14 23:29:55 +0100

    Add starter page to config

---
+++ lua/plugins/starter.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "echasnovski/mini.starter",
+    config = function()
+      local starter = require("mini.starter")
+
+      local header = {
+        "",
+        "",
+        " ███              ███             ███                  ",
+        "",
+        "████  █████ █████ ████ ████  ████ ████  █████████████  ",
+        " ███   ███  ███   ███  ███   ███  ███   ███  ███  ███  ",
+        " ███   ██████     ███  ███   ███  ███   ███  ███  ███  ",
+        " ███   ███  ███   ███   ███ ███   ███   ███  ███  ███  ",
+        "█████ ████ █████ █████   █████   █████ █████ ███ █████ ",
+        "",
+        "",
+        "|> Simple and elegant",
+        "",
+      }
+
+      starter.setup({
+        header = table.concat(header, "\n"),
+
+        items = {
+          starter.sections.builtin_actions(),
+          starter.sections.recent_files(5),
+        },
+
+        content_hooks = {
+          starter.gen_hook.aligning("center"),
+        },
+
+        footer = "",
+      })
+    end,
+  },
+}

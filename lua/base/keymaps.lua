78429510dbc7a24d371c023bc9a5e862112639fa
Mathiew82 <amateo1982@yahoo.es>
2026-01-14 21:03:53 +0100

    Add keymaps to reselect when I identing the selection

--- lua/base/keymaps.lua
+++ lua/base/keymaps.lua
@@ -1,28 +1,31 @@
 -- ------------------------------
 -- Global Keymaps
 -- ------------------------------
-vim.g.mapleader = " " -- Set Space as the leader key
+vim.g.mapleader = " "                                        -- Set Space as the leader key
 
-vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)          -- Open netrw (file explorer)
-vim.keymap.set("n", "<leader>,,", "<C-^>")             -- Jump to alternate file (previous buffer)
+vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)                -- Open netrw (file explorer)
+vim.keymap.set("n", "<leader>,,", "<C-^>")                   -- Jump to alternate file (previous buffer)
 
-vim.keymap.set("n", "<leader>,", vim.cmd.bfirst)       -- Go to first buffer
-vim.keymap.set("n", "<leader>.", vim.cmd.blast)        -- Go to last buffer
-vim.keymap.set("n", "<Tab>", vim.cmd.bnext)            -- Next buffer
-vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)      -- Previous buffer
-vim.keymap.set("n", "<leader>ls", vim.cmd.ls)          -- Show buffers list
+vim.keymap.set("n", "<leader>,", vim.cmd.bfirst)             -- Go to first buffer
+vim.keymap.set("n", "<leader>.", vim.cmd.blast)              -- Go to last buffer
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext)                  -- Next buffer
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)            -- Previous buffer
+vim.keymap.set("n", "<leader>ls", vim.cmd.ls)                -- Show buffers list
 
-vim.keymap.set("n", "<leader>o", "o<Esc>k")            -- Insert an empty line below without leaving Normal mode
-vim.keymap.set("n", "<leader>x", vim.cmd.bdelete)      -- Close the current buffer
+vim.keymap.set("n", "<leader>o", "o<Esc>k")                  -- Insert an empty line below without leaving Normal mode
+vim.keymap.set("n", "<leader>x", vim.cmd.bdelete)            -- Close the current buffer
 
-vim.keymap.set("n", "<leader>a", "ggVG")               -- Select the entire file
-vim.keymap.set("v", "<leader>y", '"+y')                -- Copy selection to system clipboard
-vim.keymap.set("n", "<leader>yy", '"+yy')              -- Copy current line to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')       -- Delete without overwriting clipboard
+vim.keymap.set("n", "<leader>a", "ggVG")                     -- Select the entire file
+vim.keymap.set("v", "<leader>y", '"+y')                      -- Copy selection to system clipboard
+vim.keymap.set("n", "<leader>yy", '"+yy')                    -- Copy current line to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')             -- Delete without overwriting clipboard
+
+vim.keymap.set("v", "<", "<gv")                              -- Reselect to continue identing the selection
+vim.keymap.set("v", ">", ">gv")                              -- Reselect to continue identing the selection
 
 vim.keymap.set("n", "<leader>dy", vim.diagnostic.open_float) -- Show diagnostics under the cursor
 
-vim.keymap.set("n", "<leader>f", "/")                  -- Start forward search
+vim.keymap.set("n", "<leader>f", "/")                        -- Start forward search
 -- Search navigation:
 --   n     = next match
 --   N     = previous match

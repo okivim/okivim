528a676f235d8aebf9ebac30e5dd0a00c7bb1943
Mathiew82 <amateo1982@yahoo.es>
2026-01-14 20:48:53 +0100

    Show project-relative file path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,14 @@ return {
           component_separators = { left = "", right = "" },
           section_separators = { left = "", right = "" },
         },
+        sections = {
+          lualine_c = {
+            {
+              "filename",
+              path = 1, -- 0 = name, 1 = relative, 2 = absolute
+            },
+          },
+        },
       })
     end,
   },

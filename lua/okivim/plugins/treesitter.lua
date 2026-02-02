5ac3e24f3cdd851dc159e110a17bb2fb9487cb2e
Mathiew82 <amateo1982@yahoo.es>
2026-02-02 18:53:21 +0100

    Refactor treesitter config and add markdown parsers

--- lua/okivim/plugins/treesitter.lua
+++ lua/okivim/plugins/treesitter.lua
@@ -4,24 +4,38 @@ return {
   build = ":TSUpdate",
   config = function()
     local parser_dir = vim.fn.stdpath("data") .. "/ts-parsers"
+    local treesitter = require("nvim-treesitter")
 
     vim.opt.rtp:prepend(parser_dir)
 
-    require("nvim-treesitter").setup({
+    treesitter.setup({
       install_dir = parser_dir,
     })
 
-    require("nvim-treesitter").install({
+    treesitter.install({
       "c", "cpp", "lua", "html", "css", "scss",
       "javascript", "typescript", "tsx",
+      "markdown", "markdown_inline",
       "vue", "yaml", "bash", "json", "toml", "php",
       "python", "rust",
     })
 
+    vim.filetype.add({
+      extension = {
+        mdx = "mdx",
+      },
+    })
+
+    vim.treesitter.language.register("markdown", "mdx")
+
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
-        "c", "h", "cpp", "cc", "cxx", "c++", "hpp", "hh", "hxx", "vue", "html", "css", "scss", "javascript", "typescript",
-        "tsx", "lua", "json", "yaml", "bash", "toml", "php", "python", "rust",
+        "c", "cpp", "lua",
+        "html", "css", "scss",
+        "javascript", "typescript", "tsx",
+        "markdown", "mdx",
+        "vue", "yaml", "bash", "json", "toml", "php",
+        "python", "rust",
       },
       callback = function()
         pcall(vim.treesitter.start)

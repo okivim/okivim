6526b85a1a746929ec03e8590aeed87302486045
Mathiew82 <amateo1982@yahoo.es>
2026-02-07 13:04:39 +0100

    Configure indentscope plugin

--- lua/okivim/plugins/indentscope.lua
+++ lua/okivim/plugins/indentscope.lua
@@ -17,6 +17,20 @@ return {
         symbol = "│",
         options = { try_as_border = true },
       })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "dashboard",
+          "alpha",
+          "starter",
+          "neo-tree",
+          "lazy",
+          "mason",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
     end,
   },
 }

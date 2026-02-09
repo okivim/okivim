bc338c6872909c6001695f974bca3a92a5abe170
Mathiew82 <amateo1982@yahoo.es>
2026-02-09 12:57:21 +0100

    Lazy-load Telescope using command trigger

--- lua/okivim/plugins/telescope.lua
+++ lua/okivim/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({})

c5914a018765305fa75c14b8707b54de7982f4a1
Mathiew82 <amateo1982@yahoo.es>
2026-02-18 22:51:16 +0100

    Simplify formatters configuration

--- lua/okivim/plugins/conform.lua
+++ lua/okivim/plugins/conform.lua
@@ -5,6 +5,13 @@ return {
     config = function()
       require("conform").setup({
         formatters_by_ft = {
+          --
+          -- The following formatters are not installed by default
+          -- You can install them using Mason (if available) or manually
+          -- Install the ones you need :)
+          --
+          sh              = { "shfmt" },
+          bash            = { "shfmt" },
           javascript      = { "prettierd", "prettier" },
           typescript      = { "prettierd", "prettier" },
           javascriptreact = { "prettierd", "prettier" },
@@ -18,19 +25,18 @@ return {
           php             = { "pint" },
           python          = { "black" },
           rust            = { "rustfmt" },
-          sh              = { "shfmt" },
-          bash            = { "shfmt" },
           toml            = { "taplo" },
-        },
-
-        formatters = {
-          stylelint = {
-            timeout_ms = 3000,
-          },
+          --
+          -- Add here any formatters you need
+          -- For example: java = { "google-java-format" }
+          --
+          -- Filetype names must match Neovim's filetypes (e.g. "javascriptreact", "typescriptreact").
+          -- You can check the current filetype with :set filetype?
+          -- See :help filetype for more information.
         },
 
         format_on_save = {
-          timeout_ms = 2000,
+          timeout_ms = 3000,
           lsp_fallback = true,
         },
       })

b0cf492b2c3e5071b12b2ae1281e8e64a907e975
Mathiew <amateo1982@yahoo.es>
2026-02-24 20:34:46 +0100

    Refactor Neovim options for improved usability

--- lua/okivim/config/options.lua
+++ lua/okivim/config/options.lua
@@ -1,30 +1,55 @@
-vim.opt.number = true             -- Show line numbers
-vim.opt.relativenumber = true     -- Show relative line numbers
+vim.opt.termguicolors = true                         -- Enable 24-bit colors
+vim.opt.updatetime = 250                             -- Reduce refresh time (snappy)
+vim.opt.ttimeoutlen = 0                              -- Key code timeout
+vim.opt.autoread = true                              -- Auto-reload changes if outside of neovim
+vim.opt.autowrite = false                            -- Do not auto-save
 
-vim.opt.splitright = true         -- Split vertically right
-vim.opt.splitbelow = true         -- Split below
+vim.opt.signcolumn = "yes"                           -- Always show sign column
+vim.opt.clipboard = "unnamedplus"                    -- Enable system clipboard
 
-vim.opt.tabstop = 2               -- A tab equals 2 spaces
-vim.opt.shiftwidth = 2            -- Indentation size when using >>
-vim.opt.expandtab = true          -- Convert tabs to spaces
+vim.opt.number = true                                -- Show line numbers
+vim.opt.relativenumber = true                        -- Show relative line numbers
+vim.opt.cursorline = true                            -- Highlight current line
+vim.opt.wrap = true                                  -- Wrap lines by default
+vim.opt.scrolloff = 10                               -- Keep 10 lines above/below cursor
 
-vim.opt.smartindent = true        -- Smart automatic indentation
-vim.opt.clipboard = "unnamedplus" -- Enable system clipboard
+vim.opt.splitright = true                            -- Split vertically right
+vim.opt.splitbelow = true                            -- Split below
 
-vim.opt.swapfile = false          -- Disable swap files (less I/O)
-vim.opt.backup = false            -- Do not create backup files
-vim.opt.undofile = true           -- Persistent undo across sessions
+vim.opt.ignorecase = true                            -- Case insensitive search
+vim.opt.smartcase = true                             -- Case sensitive if uppercase in string
+vim.opt.hlsearch = true                              -- Highlight search matches
+vim.opt.incsearch = true                             -- Show matches as you type
 
-vim.opt.updatetime = 250          -- Reduce refresh time (snappy)
+vim.opt.tabstop = 2                                  -- Tabwidth
+vim.opt.shiftwidth = 2                               -- Indent width
+vim.opt.softtabstop = 2                              -- Soft tab stop not tabs on tab/backspace
+vim.opt.expandtab = true                             -- Use spaces instead of tabs
+vim.opt.smartindent = true                           -- Smart auto-indent
+vim.opt.autoindent = true                            -- Copy indent from current line
 
-vim.opt.termguicolors = true      -- Enable 24-bit colors
-vim.opt.scrolloff = 8             -- Keep 8 lines visible around cursor
-vim.opt.signcolumn = "yes"        -- Always show sign column
+vim.opt.swapfile = false                             -- Disable swap files (less I/O)
+vim.opt.backup = false                               -- Do not create backup files
+vim.opt.undofile = true                              -- Persistent undo across sessions
 
-vim.opt.foldmethod = "syntax"     -- Create folds based on syntax highlighting
-vim.opt.foldlevelstart = 99       -- Start with all folds open
+vim.opt.showmatch = true                             -- Highlights matching brackets
+vim.opt.cmdheight = 1                                -- Single line command line
+vim.opt.completeopt = "menuone,noinsert,noselect"    -- Completion options
+vim.opt.showmode = false                             -- Do not show the mode, instead have it in statusline
+vim.opt.pumheight = 10                               -- Popup menu height
+vim.opt.pumblend = 10                                -- Popup menu transparency
+vim.opt.winblend = 0                                 -- Floating window transparency
+vim.opt.conceallevel = 0                             -- Do not hide markup
+vim.opt.concealcursor = ""                           -- Do not hide cursorline in markup
+vim.opt.hidden = true                                -- Allow hidden buffers
 
-vim.opt.path:append("**")         -- Search files recursively in all subdirectories when using :find or gf
+vim.opt.fillchars = { eob = " " }                    -- Hide "~" on empty lines
+
+vim.opt.foldmethod = "expr"                          -- Use expression for folding
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folding
+vim.opt.foldlevelstart = 99                          -- Start with all folds open
+
+vim.opt.path:append("**")                            -- Search files recursively in all subdirectories when using :find or gf
 vim.opt.suffixesadd:append({
   ".ts",
   ".tsx",
@@ -33,4 +58,4 @@ vim.opt.suffixesadd:append({
   ".json",
   ".mjs",
   ".cjs",
-})                                -- Automatically try these file extensions when resolving files (e.g., with gf or :find)
+}) -- Automatically try these file extensions when resolving files (e.g., with gf or :find)

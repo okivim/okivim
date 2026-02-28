37053a60c50a2284be622ac7f32e2fe7ef26717c
Mathiew <amateo1982@yahoo.es>
2026-02-28 15:09:34 +0100

    Create autocommand to delete conform.log on start
    Add an autocommand to delete conform.log on VimEnter.

--- lua/okivim/config/autocmds.lua
+++ lua/okivim/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- ------------------------------
 -- Global Auto cmd's
 -- ------------------------------
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local log = vim.fn.stdpath("data") .. "/conform.log"
+
+    if vim.fn.filereadable(log) == 1 then
+      vim.fn.delete(log)
+    end
+  end,
+})

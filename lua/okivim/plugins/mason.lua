65d03467d31c31245f544ad818ac38d9aa4930e3
Mathiew <amateo1982@yahoo.es>
2026-03-01 05:41:52 +0100

    Change event trigger for mason-lspconfig to VeryLazy

--- lua/okivim/plugins/mason.lua
+++ lua/okivim/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    event = "VeryLazy",
     dependencies = {
       "mason-org/mason.nvim",
       "neovim/nvim-lspconfig",
